4bc87dd4a1bb4abc0cde357cbfe0fabb4bb33f72
Ashish <62475101+ashish9921@users.noreply.github.com>
2023-10-07 19:51:55 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import cors from 'cors';
+import session from 'session',
 
 dotenv.config();
 
