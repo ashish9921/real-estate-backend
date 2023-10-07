d5634a12393a19c7647ad7d3f9942870875260a5
Ashish <62475101+ashish9921@users.noreply.github.com>
2023-10-07 19:40:26 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -40,7 +40,7 @@ export const google = async (req, res, next) => {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = user._doc;
       res
-        .cookie('access_token', token, { httpOnly: true })
+        .cookie('access_token', token, { secure: true })
         .status(200)
         .json(rest);
     } else {
