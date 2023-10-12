91565555dffd144a2478875610233f8de8c43a42
Ashish <62475101+ashish9921@users.noreply.github.com>
2023-10-12 11:02:11 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -40,7 +40,6 @@ app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
