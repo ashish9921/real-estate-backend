d8d1069fe5fba9d65ec5d7250e5c27866ab97bb9
Ashish <62475101+ashish9921@users.noreply.github.com>
2023-10-18 23:05:40 +0530

    Update Listing.jsx

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -32,7 +32,7 @@ export default function Listing() {
     const fetchListing = async () => {
       try {
         setLoading(true);
-        const res = await fetch(`https://real-estate-nemg.onrender.com/api/listing/get/${params.listingId}`);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
         const data = await res.json();
         if (data.success === false) {
           setError(true);
