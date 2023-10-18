db74c82342862694c09f4639db8a074e1322e3e1
Ashish <62475101+ashish9921@users.noreply.github.com>
2023-10-18 23:00:29 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,29 +3,19 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-//   server: {
-//     proxy: {
+  server: {
+    proxy: {
         
-//       '/api': {
-//         target: 'https://real-estate-nemg.onrender.com/',
-//         changeOrigin:true,
+      '/api': {
+        target: 'http://localhost:3000',
+        secure:false,
         
-//         ewrite: (path) => path.replace(/^\/api/, ''),
         
-//       },
-//     },
-//   },
+        
+      },
+    },
+  },
 
   plugins: [react()],
-  build: {
-    rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-},
+ 
 });
