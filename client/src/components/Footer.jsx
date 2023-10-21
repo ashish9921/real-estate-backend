78ef7b6700e5ed85237eb5ef4a36a1f636e1f3f7
Ashish <62475101+ashish9921@users.noreply.github.com>
2023-10-21 15:34:51 +0530

    Add files via upload

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bottom-0 absolute w-full'>
+    <div className=' bg-slate-800 p-3 '>
+        <div className='justify-center flex flex-row gap-2  '>
+            <h1 className='text-slate-200'>Created By</h1>
+            <span className='text-cyan-500 uppercase'>Ashish</span>
+            
+        </div>
+    </div>
+    </footer>
+  )
+}
